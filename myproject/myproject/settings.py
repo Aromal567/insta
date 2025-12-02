aac3e32b541c869f756a02475aba6b0464a33606
Aromal567 <aromalbabu9847@gmail.com>
2025-12-02 21:36:42 +0530

    Updated apps.py ,settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myproject.myapp'
+    'myproject.myapp.apps.MyappConfig',
 ]
 
 MIDDLEWARE = [
