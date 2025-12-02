7e5d8e48e8f9e538384e479cb49a2dec41a239a5
Aromal567 <aromalbabu9847@gmail.com>
2025-12-02 22:03:22 +0530

    manage.py new

--- myproject/manage.py
+++ myproject/manage.py
@@ -6,17 +6,25 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.myproject.settings")
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(BASE_DIR, "myproject"))
+
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "myproject.myproject.settings"
+    )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Make sure it is installed and "
+            "your virtual environment is activated."
         ) from exc
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
