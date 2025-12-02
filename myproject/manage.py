f88050d9905f84900dc62c9ed73b815aa71383d7
Aromal567 <aromalbabu9847@gmail.com>
2025-12-02 22:38:59 +0530

    new

--- myproject/manage.py
+++ myproject/manage.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
-
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(BASE_DIR)
     sys.path.append(os.path.join(BASE_DIR, "myproject"))
 
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "myproject.myproject.settings"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.myproject.settings")
 
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Make sure it is installed and "
-            "your virtual environment is activated."
-        ) from exc
-
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
 
 if __name__ == "__main__":
     main()
--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -1,15 +1,14 @@
-"""
-WSGI config for myproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+OUTER_DIR = os.path.dirname(BASE_DIR)
+
+# Make sure Django can find myapp
+sys.path.append(BASE_DIR)     # inner myproject
+sys.path.append(OUTER_DIR)    # outer folder
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.myproject.settings")
 
